c2265848fe13b62a8b0bfa4e30bf65f933bff788
Artyom.Antipov <Artyom.Antipov@nedra.digital>
2021-07-11 21:19:26 +0300

    Add spring web starter

--- src/main/java/ru/netology/moneytransferservice/rest/ConfirmOperationController.java
+++ src/main/java/ru/netology/moneytransferservice/rest/ConfirmOperationController.java
@@ -4,7 +4,9 @@ import org.openapitools.api.ConfirmOperationApi;
 import org.openapitools.model.InlineObject1;
 import org.openapitools.model.InlineResponse200;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConfirmOperationController implements ConfirmOperationApi {
   @Override
   public ResponseEntity<InlineResponse200> confirmOperationPost(InlineObject1 inlineObject1) {
--- src/main/java/ru/netology/moneytransferservice/rest/TransferController.java
+++ src/main/java/ru/netology/moneytransferservice/rest/TransferController.java
@@ -4,7 +4,9 @@ import org.openapitools.api.TransferApi;
 import org.openapitools.model.InlineObject;
 import org.openapitools.model.InlineResponse200;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class TransferController implements TransferApi {
   @Override
   public ResponseEntity<InlineResponse200> transferPost(InlineObject inlineObject) {
